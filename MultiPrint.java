a53f327ef2cfef79fdd4ab1d06229dd72b18b302
Safayatul1 <165216434+Safayatul1@users.noreply.github.com>
2024-03-27 14:40:42 -0700

    Add files via upload

---
+++ MultiPrint.java
@@ -0,0 +1,10 @@
+public class MultiPrint {
+    public static void main(String[] args) {
+        System.out.println("first");
+        System.out.println("second");
+        System.out.println("third");
+        System.out.println("fourh");
+        System.out.println("fifth");   
+            
+            }
+}
---
+++ PerimeterCalculation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PerimeterCalculation {
+    public static void main(String[] args) {
+        // TODO: write your code below this comment
+    Scanner in = new Scanner(System.in);
+        System.out.print("Enter width: ");
+        int first = in.nextInt();
+        System.out.print("Enter height: ");
+        int second = in.nextInt();
+        int added = (first + second)*2;
+        System.out.print("Perimeter: ");
+        System.out.println(added);
+    
+    
+    
+    }
+}
